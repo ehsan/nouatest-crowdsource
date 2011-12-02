6b1bb76715e13a1e7b926220c66421f432f66a36
Ehsan Akhgari <ehsan.akhgari@gmail.com>
2011-12-01 20:02:18 -0500

    Add some more code

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-TEST_PACKAGE=http://people.mozilla.org/~eakhgari/nouatest-crowdsource.zip
+FILE=nouatest-crowdsource.zip
+TEST_PACKAGE=http://people.mozilla.org/~eakhgari/$FILE
+if [ "x$TMP" == x ]; then
+  TMP=/tmp
+fi
+DIR="$TMP/nouactest-crowdsource"
 
 if [ "x$1" == x ]; then
   "$0" foo | tee log.txt
@@ -24,10 +29,24 @@ run_checked_command() {
   rm -f stdout stderr
 }
 
-run_checked_command echo foo
+if [ -d "$DIR" ]; then
+  run_checked_command rm -rf "$DIR"
+fi
+run_checked_command mkdir "$DIR"
+cd "$DIR"
 
 echo Downloading the test package...
 run_checked_command wget -N "$TEST_PACKAGE"
 
+echo Extracting the test package...
+run_checked_command unzip -o "$FILE"
 
+echo Preparing to run the tests...
+
+# install the service
+run_checked_command elevatedRunner.exe
+# wait for the installer to finish
+while ps -aW | grep -iq installer.exe; do
+  sleep 1
+done
 
